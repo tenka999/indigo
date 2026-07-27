25b45213de84a8d6b36d6d36811b033581b386a7
tenka999 <keyzadepok95@gmail.com>
2026-07-27 22:36:19 +0700

    create hero section

--- frontend/src/presentation/views/home/section/HeroSection.jsx
+++ frontend/src/presentation/views/home/section/HeroSection.jsx
@@ -1,79 +1,43 @@
-import React from "react";
 import "@/styles/hero-style.css";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
 export default function HeroSection() {
-  useGSAP(() => {
-    gsap.to(".hero-headline h1", {
-      y: "0",
-
-      scrollTrigger: {
-        trigger: ".hero-headline",
-        start: "top bottom",
-        end: "top top",
-        scrub: true,
-      },
-    });
-    gsap.to(".hero-box-image", {
-      // scale: 1.8,
-      y: "300px",
-
-      scrollTrigger: {
-        trigger: ".about-section",
-        start: "top bottom",
-        end: "top top",
-        scrub: true,
-      },
-    });
-    gsap.to(".hero-image", {
-      scale: 1.8,
-      y: "-200px",
-
-      scrollTrigger: {
-        trigger: ".about-section",
-        start: "top bottom",
-        end: "top top",
-        scrub: true,
-      },
-    });
-    gsap.from(".hero-headline > *", {
-      y: 100,
-      opacity: 0,
-      duration: 0.4,
-      stagger: 0.3,
-      ease: "power4.out",
-
-      scrollTrigger: {
-        trigger: ".hero-headline ",
-        start: "top 80%",
-      },
-    });
-  });
   return (
     <div className="hero-section">
-      <div className="hero-content">
-        <div className="hero-headline">
-          <h1 className="hero-headline-1">BUILT TO ENDURE</h1>
-          <h1 className="hero-headline-2">MONOLITHIC STRUCTURES</h1>
+      <div className="hero-top">
+        <div className="hero-paragraph">
+          We craft websites made for people, not just algorithms. By blending
+          clean design with intuitive structure, we build calm digital spaces
+          where your brand can genuinely connect with your clients.
         </div>
-        <div className="hero-sub-headline">
-          <p>
-            Rooted in industrial scale. KRAFT & FORM shapes solid wood,
-            structural steel, and raw concrete into monumental spaces.
-          </p>
+        <div className="hero-cta">
+          Say Hello {""}
+          <div className="cta-arrow-container">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              className="cta-arrow"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5 12H19M19 12L13 6M19 12L13 18"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
         </div>
-        <button className="hero-cta-button">VIEW MONUMENTS</button>
       </div>
-      <div className="hero-container-image">
-        <div className="hero-box-left"></div>
-        <div className="hero-box-image">
-          <div className="hero-image"></div>
+      <div className="hero-bottom">
+        <div className="hero-brand">
+          Clarity Studio <p className="hero-r">R</p>
         </div>
-        <div className="hero-box-right"></div>
+        <div className="hero-rights">
+          <p>&copy; 2026 All Rights Reserved</p>
+          <p>(Scroll down)</p>
+        </div>
       </div>
     </div>
   );
