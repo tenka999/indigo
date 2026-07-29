f5e47d52232e6af024cd5cf64af0a13b31f5420c
tenka999 <keyzadepok95@gmail.com>
2026-07-29 16:51:54 +0700

    create hero section

--- frontend/src/presentation/views/home/HomePage.jsx
+++ frontend/src/presentation/views/home/HomePage.jsx
@@ -1,14 +1,12 @@
-import Navbar from "./layout/Navbar";
-import "@/styles/home-sytle.css";
+import "@/styles/home-style.css";
 import { useRef, useState } from "react";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
-gsap.registerPlugin(ScrollTrigger);
+import HeroSection from "./section/HeroSection";
 // import { ReactComponent as Logo } from "/layout/background/svg-path.svg";
 
-gsap.registerPlugin(MorphSVGPlugin, ScrollTrigger);
 export default function HomePage() {
-  return <div className="body">asdas</div>;
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
 }
