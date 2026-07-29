a34bdf506e18ac55a2a366db3ceb9ba637427c21
tenka999 <keyzadepok95@gmail.com>
2026-07-29 23:33:46 +0700

    create product section

--- frontend/src/presentation/views/home/HomePage.jsx
+++ frontend/src/presentation/views/home/HomePage.jsx
@@ -1,12 +1,14 @@
 import "@/styles/home-style.css";
 import { useRef, useState } from "react";
 import HeroSection from "./section/HeroSection";
+import ProductSection from "./section/ProductSection";
 // import { ReactComponent as Logo } from "/layout/background/svg-path.svg";
 
 export default function HomePage() {
   return (
     <div>
       <HeroSection />
+      <ProductSection />
     </div>
   );
 }
